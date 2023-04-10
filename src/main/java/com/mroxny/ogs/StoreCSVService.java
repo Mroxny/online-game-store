1cf059a648d0d6ce81d029700c2f0d6a39249ae0
Mroxny <Maciej.h.mrozek@gmail.com>
2023-04-11 01:26:50 +0200

    Implement basic interface

--- src/main/java/com/mroxny/ogs/StoreCSVService.java
+++ src/main/java/com/mroxny/ogs/StoreCSVService.java
@@ -1,4 +1,10 @@
 package com.mroxny.ogs;
 
+import java.util.List;
+
 public class StoreCSVService implements StoreDBInterface{
+    @Override
+    public List<Game> getAllGames() {
+        return null;
+    }
 }
--- src/main/java/com/mroxny/ogs/StoreDBInterface.java
+++ src/main/java/com/mroxny/ogs/StoreDBInterface.java
@@ -1,5 +1,8 @@
 package com.mroxny.ogs;
 
+import java.util.List;
+
 public interface StoreDBInterface {
-    
+    public List<Game> getAllGames();
+
 }
