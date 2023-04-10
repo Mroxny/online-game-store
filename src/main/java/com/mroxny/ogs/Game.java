dd7b4e4441902466e72b96a6f623831429b8c325
Mroxny <Maciej.h.mrozek@gmail.com>
2023-04-11 00:50:49 +0200

    Create Game.java

---
+++ src/main/java/com/mroxny/ogs/Game.java
@@ -0,0 +1,19 @@
+package com.mroxny.ogs;
+
+import java.util.List;
+
+public class Game {
+    private int id;
+    private String name;
+    private String cover;
+    private String smallDesc;
+    private String bigDesc;
+    private String premiere;
+    private float price;
+    private List<String> trailers;
+    private List<String> photos;
+    private List<String> genres;
+    private List<String> tags;
+    
+
+}
