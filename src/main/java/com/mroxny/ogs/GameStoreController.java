188c6b1d306b7eef5f7952f2725b6f5043dc77a0
Mroxny <Maciej.h.mrozek@gmail.com>
2023-04-11 01:19:19 +0200

    Update GameStoreController.java

--- src/main/java/com/mroxny/ogs/GameStoreController.java
+++ src/main/java/com/mroxny/ogs/GameStoreController.java
@@ -1,5 +1,6 @@
 package com.mroxny.ogs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/store")
 public class GameStoreController {
+
+    private StoreDBInterface dbManager;
+
+    @Autowired
+    public GameStoreController(StoreDBInterface storeDBInterface){
+        this.dbManager = storeDBInterface;
+    }
 
     @GetMapping("/getGames")
     public Game getGames(){
