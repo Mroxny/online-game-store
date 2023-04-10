dab2f20dc3e9fabc2e0032a88f64545c1c537b98
Mroxny <Maciej.h.mrozek@gmail.com>
2023-04-11 00:54:56 +0200

    Create Requirements.java

---
+++ src/main/java/com/mroxny/ogs/Requirements.java
@@ -0,0 +1,58 @@
+package com.mroxny.ogs;
+
+public class Requirements {
+    private int id;
+    private String system;
+    private String processor;
+    private String memory;
+    private String graphics;
+    private String storage;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSystem() {
+        return system;
+    }
+
+    public void setSystem(String system) {
+        this.system = system;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public String getMemory() {
+        return memory;
+    }
+
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    public String getGraphics() {
+        return graphics;
+    }
+
+    public void setGraphics(String graphics) {
+        this.graphics = graphics;
+    }
+
+    public String getStorage() {
+        return storage;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+}
