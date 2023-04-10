0a33c18dd411551ad5586b49564ee154f3f41fe2
Mroxny <Maciej.h.mrozek@gmail.com>
2023-04-11 00:59:27 +0200

    Update Game.java

--- src/main/java/com/mroxny/ogs/Game.java
+++ src/main/java/com/mroxny/ogs/Game.java
@@ -14,6 +14,110 @@ public class Game {
     private List<String> photos;
     private List<String> genres;
     private List<String> tags;
+    private Studio studio;
+    private Requirements requirements;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getSmallDesc() {
+        return smallDesc;
+    }
+
+    public void setSmallDesc(String smallDesc) {
+        this.smallDesc = smallDesc;
+    }
+
+    public String getBigDesc() {
+        return bigDesc;
+    }
+
+    public void setBigDesc(String bigDesc) {
+        this.bigDesc = bigDesc;
+    }
+
+    public String getPremiere() {
+        return premiere;
+    }
+
+    public void setPremiere(String premiere) {
+        this.premiere = premiere;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public List<String> getTrailers() {
+        return trailers;
+    }
+
+    public void setTrailers(List<String> trailers) {
+        this.trailers = trailers;
+    }
+
+    public List<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<String> photos) {
+        this.photos = photos;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Studio getStudio() {
+        return studio;
+    }
+
+    public void setStudio(Studio studio) {
+        this.studio = studio;
+    }
+
+    public Requirements getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(Requirements requirements) {
+        this.requirements = requirements;
+    }
 }
