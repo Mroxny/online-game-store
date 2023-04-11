04fd40e5b8efdf242ff96ca30dd8973d5cc3331a
Mroxny <Maciej.h.mrozek@gmail.com>
2023-04-11 18:04:44 +0200

    Update StoreCSVService.java

--- src/main/java/com/mroxny/ogs/StoreCSVService.java
+++ src/main/java/com/mroxny/ogs/StoreCSVService.java
@@ -15,6 +15,11 @@ public class StoreCSVService implements StoreDBInterface{
     private static String FILE_IMAGES="./csvDB/images.csv";
     private static String FILE_STUDIOS="./csvDB/studios.csv";
     private static String FILE_REQUIREMENTS="./csvDB/requirements.csv";
+    private static String FILE_GENRES="./csvDB/genres.csv";
+    private static String FILE_TAGS="./csvDB/tags.csv";
+    private static String FILE_GAMES_GENRES="./csvDB/games_genres.csv";
+    private static String FILE_GAMES_TAGS="./csvDB/games_tags.csv";
+
 
 
     private List<String> lines;
@@ -54,12 +59,35 @@ public class StoreCSVService implements StoreDBInterface{
         game.setBigDesc(values[4]);
         game.setPremiere(values[5]);
         game.setPrice(Float.parseFloat(values[6]));
-        game.setTrailers(getOneToMany(FILE_GAMES, game.getId()));
+        game.setTrailers(getOneToMany(FILE_TRAILERS, game.getId()));
         game.setPhotos(getOneToMany(FILE_IMAGES, game.getId()));
         game.setStudio(Studio.getFromCSV(getObjectLineById(FILE_STUDIOS, Integer.parseInt(values[7]))));
         game.setRequirements(Requirements.getFromCSV(getObjectLineById(FILE_REQUIREMENTS, Integer.parseInt(values[8]))));
+        game.setGenres(getManyToMany(FILE_GAMES_GENRES,FILE_GENRES, game.getId()));
+        game.setTags(getManyToMany(FILE_GAMES_TAGS,FILE_TAGS, game.getId()));
 
         return game;
+    }
+
+    private List<String> getManyToMany(String path1, String path2, int id){
+        List<String> lines1 = readCSV(path1);
+        List<String> lines2 = readCSV(path2);
+        List<String> res = new ArrayList<>();
+
+        for(String s1 : lines1){
+            String[] vals1 = s1.split(",");
+            int idFromFile1 = Integer.parseInt(vals1[0]);
+            if(idFromFile1 == id){
+                int target = Integer.parseInt(vals1[1]);
+                for(String s2 : lines2){
+                    String[] vals2 = s2.split(",");
+                    int idFromFile2 = Integer.parseInt(vals2[0]);
+                    if(idFromFile2 == target)res.add(vals2[1]);
+
+                }
+            }
+        }
+        return res;
     }
 
     private List<String> getOneToMany(String path, int id){
